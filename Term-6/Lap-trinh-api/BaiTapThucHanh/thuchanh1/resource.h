0c80c8ee837105d37f6d6bbc173a36dd6b7f9002
Harry <hoangnguyen72.dev@gmail.com>
2026-03-13 20:45:44 +0700

    update

---
+++ Term-6/Lap-trinh-api/BaiTapThucHanh/thuchanh1/resource.h
@@ -0,0 +1,39 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by thuchanh1.rc
+//
+#define IDC_MYICON                      2
+#define IDD_THUCHANH1_DIALOG            102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_THUCHANH1                   107
+#define IDI_SMALL                       108
+#define IDC_THUCHANH1                   109
+#define IDR_MAINFRAME                   128
+#define IDR_MENU1                       129
+#define IDR_MENU2                       130
+#define ID_TAMGIACVUONG_DUONGTHANG      32771
+#define ID_H32772                       32772
+#define IDC_TGV                         32773
+#define ID_DT                           32774
+#define ID_TGV                          32775
+#define ID_MAUNEN_RED                   32776
+#define ID_MAUNEN_GREEN                 32777
+#define ID_DO                           32778
+#define ID_XANH                         32779
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32780
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+
+#endif
